1fcc321617b57d6df26b052f3b658b99296630b6
Minifyre <minifyre@gmail.com>
2018-11-12 18:11:06 -0600

    +logic.edit
    Logic edit handles updating that state as to which elment, if any is being edited by the user.

--- logic.js
+++ logic.js
@@ -1,6 +1,7 @@
+logic.edit=(state,id='')=>state.view.edit=id
+logic.item=(...opts)=>util.mk({complete:false,text:'',list:[]},...opts)
 logic.normalize=function(state)
 {
 	if(!state.file.data.index) state.file.data.index=logic.item({id:'index'})
 	return state
 }
-logic.item=(...opts)=>util.mk({complete:false,text:'',list:[]},...opts)
