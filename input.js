82ad37f233e0a6a63ea75632878b20385b9a84ae
Minifyre <minifyre@gmail.com>
2018-11-12 18:13:12 -0600

    +input.blur
    Input blur triggers after the editable element is removed & if it contains no non-whitespace content, it is removed.

--- input.js
+++ input.js
@@ -6,7 +6,13 @@ input.back=function(state,evt)
 {
 
 }
+input.blur=function(state,{target})
+{
+	if(util.empty(target.innerText)) logic.remove(state,target.id)
+
+	logic.edit(state)
+}
 input.open=function(state,evt)
 {
-	
+
 }
