8fcb249e73ac7f5a58526dd84304e95aa046f21c
Minifyre <minifyre@gmail.com>
2018-11-12 18:12:06 -0600

    +logic.remove
    Unlinks references from the item to be removed and the item itself (will handle removing children in the future).

--- logic.js
+++ logic.js
@@ -5,3 +5,19 @@ logic.normalize=function(state)
 	if(!state.file.data.index) state.file.data.index=logic.item({id:'index'})
 	return state
 }
+logic.remove=function(state,id)
+{
+	//@todo delete all children
+	//@todo if items chan have multiple parents, this needs to be overhauled
+
+	//delete links to item
+	Object.values(state.file.data)
+	.forEach(function({list})
+	{
+		const i=list.indexOf(id)
+
+		if(i!==-1) list.splice(i,1)
+	})
+
+	delete state.file.data[id]
+}
