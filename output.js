82159729b50d9fdd1507c596e04aa3d4535047a0
Minifyre <minifyre@gmail.com>
2018-11-13 13:21:26 -0600

    bug fix
    Fixed issue that occured when pop was called on state.view.path (an extra empty item on the end was not filtered out).

--- output.js
+++ output.js
@@ -3,13 +3,14 @@ output.render=function(state)
 	const
 	[blur,pointerup]=[input.blur,input].map(fn=>evt=>fn(state,evt)),
 	lists=['index',...state.view.path]
+	//an empty item exists on the end after a splice/pop call, b4 length is changed
+	.filter(x=>!!x)
 	.map(function(listId,i,path)
 	{
 		const
 		item=state.file.data[listId],
 		selected=path[i+1],
 		items=item.list
-		//an empty item exists on the end after a splice call, b4 length is changed
 		.filter(x=>!!x)
 		.map(function(id)
 		{
