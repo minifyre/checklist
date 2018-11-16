9f0148937a59f757a69674aa59c4414f2ea077f8
Minifyre <minifyre@gmail.com>
2018-11-16 14:43:54 -0600

    bug fix
    Shuffle function was not working as it had an extra `arr` parameter in the front (a hold over from a previous reduce-based version).

--- util.js
+++ util.js
@@ -8,8 +8,8 @@ util.mapEmpty=(arr,fn)=>arr.filter(x=>!!x).map(fn)
 util.rand=(max,min=1,seed=Math.random())=>Math.floor(seed*(max-min+1))
 util.shuffle=function(arr)
 {
-	arr.map(()=>util.rand(arr.length)-1)//-1 makes this zero-based
-	.forEach((arr,j,i)=>[arr[i],arr[j]]=[arr[j],arr[i]])
+	arr.map(()=>util.rand(arr.length-1,0))
+	.forEach((j,i)=>[arr[i],arr[j]]=[arr[j],arr[i]])
 
 	return arr
 }
