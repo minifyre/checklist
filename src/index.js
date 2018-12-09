33ffff2b449603aaf4c07a2e716d9bad9aebfedf
Minifyre <minifyre@gmail.com>
2018-12-09 17:38:09 -0600

    used sio to assign properties

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import v from './node_modules/v/v.mjs'
 
 const {config,util,logic,output,input}=silo
 
-export default Object.assign(async function init(initialState)
+export default silo(async function init(initialState)
 {
 	const
 	state=logic(initialState),
 	render=truth.compile(({state})=>v.render(document.body,state,output))
 
 	truth(state,render)
-},silo)
+})
