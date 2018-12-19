4f52a77cde11410341ac9de2da5ee66a908084c5
Minifyre <minifyre@gmail.com>
2018-12-19 16:50:02 -0600

    moved list files into standalone list module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import silo from './node_modules/silo/index.js'
+import silo from './node_modules/list/index.js'
 import truth from './node_modules/truth/truth.mjs'
 import v from './node_modules/v/v.mjs'
 
@@ -6,11 +6,4 @@ const
 {config,util,logic,output,input}=silo,
 {curry}=util
 
-export default silo(async function init(initialState)
-{
-	const
-	state=logic(initialState),
-	render=truth.compile(({state})=>v.render(document.body,state,output))
-
-	truth(state,render)
-})
+export default silo
