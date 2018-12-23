e853e077047c5874d4c57751e2340b976d7b3c78
Minifyre <minifyre@gmail.com>
2018-12-23 17:32:48 -0600

    +extended config.header

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
+config.header.item=['complete','repeat',...config.header.item]
+config.header.list=[...config.header.list.slice(0,-1),'shuffle','download','add']
 config.item.complete=false
 config.state.view.theme=['red','darkorange','#fc0']
