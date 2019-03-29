e5ef42cb3cf919cc20d4ec97b84f096eab40b9ec
Minifyre <minifyre@gmail.com>
2019-03-29 10:57:01 -0500

    +config.state.view.type

--- src/config.js
+++ src/config.js
@@ -2,3 +2,4 @@ config.header.item=['complete','repeat',...config.header.item]
 config.header.list=[...config.header.list.slice(0,-1),'repeat','shuffle','download','add']
 config.item.complete=false
 config.state.view.theme=['red','darkorange','#fc0']
+config.state.view.type='check-list'
